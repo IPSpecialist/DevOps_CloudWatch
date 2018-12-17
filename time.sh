697d5d7cd6ae441b877b74d6c0ccd34219a9d730
IPSpecialist <43603376+IPSpecialist@users.noreply.github.com>
2018-12-17 11:21:46 +0500

    Add files via upload

---
+++ time.sh
@@ -0,0 +1,2 @@
+export AWS_DEFAULT_REGION=us-east-1
+/usr/local/bin/aws cloudwatch put-metric-data --metric-name CurrentUnixTime --namespace "IPSpecialist" --value `date +%s` --timestamp `date --utc "+%FT%T.%N" | sed -r 's/[[:digit:]]{6}$/Z/'`
